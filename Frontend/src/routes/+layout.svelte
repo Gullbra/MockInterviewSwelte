f98ddceb811eaf19a7d9e777dbfaaa6ef2f6dbc0
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-11-22 14:36:33 +0100

    state, html and css for questions. Checkpoint befor

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,45 +1,47 @@
 <script lang="ts">
   import '../styles/base.css'
   import '../styles/colors.css'
+  import '../styles/fonts.css'
 </script>
 
 
 <header class="site__header">
-  <h1><a href="/">Site Title</a></h1>
+  <h1><a href="/">Site Title E</a></h1>
   <nav class="header__links-container">
-    <nav class="links-container__dropdown-container">
+    <nav class="links-container__dropdown-container dev-border">
       <h4 class="links-container__dropdown-header">Site Navigation</h4>
+      
       <div class="links-container__dropdown-menu">
-        <a class="dropdown-menu__dropdown-element" href="/preparation">/preparation</a>
-        <a class="dropdown-menu__dropdown-element" href="/web-cam-test">/webcam test</a>
-        <a class="dropdown-menu__dropdown-element" href="/about">/about</a>
+        <p class="dropdown-menu__dropdown-element"><a href="/preparation">/preparation</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="/web-cam-test">/webcam test</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="/about">/about</a></p>
       </div>
     </nav>
     <nav class="links-container__dropdown-container">
       <h4 class="links-container__dropdown-header">Other Tutorials</h4>
       <div class="links-container__dropdown-menu">
-        <a class="dropdown-menu__dropdown-element" href="https://dev.to/nelsoncode/how-to-use-the-webcam-with-svelte-js-2415">Webcam + Svelte</a>
-        <a class="dropdown-menu__dropdown-element" href="https://codepen.io/martinwolf/pen/eNNdme">Links css</a>
-        <a class="dropdown-menu__dropdown-element" href="https://colorhunt.co/palette/fefbe9e1eeddf0a04b183a1d">Color palette</a>
+        <p class="dropdown-menu__dropdown-element"><a href="https://dev.to/nelsoncode/how-to-use-the-webcam-with-svelte-js-2415">Webcam + Svelte</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://codepen.io/martinwolf/pen/eNNdme">Links css</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://colorhunt.co/palette/fefbe9e1eeddf0a04b183a1d">Color palette</a></p>
       </div>
     </nav>
     <nav class="links-container__dropdown-container">
       <h4 class="links-container__dropdown-header">Svelte Tutorial</h4>
       <div class="links-container__dropdown-menu">
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/welcome-to-svelte">Svelte tutorial</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/reactive-declarations">Svelte states</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/declaring-props">Svelte props and imports</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/default-values">Svelte default props</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/spread-props">Svelte spread props</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/else-if-blocks">Svelte if/else</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/each-blocks">Svelte loops/maps</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/keyed-each-blocks">Svelte keyed each</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/await-blocks">Svelte promise-handling</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/dom-events">Svelte events</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/event-modifiers">Svelte event modifiers</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/component-events">Svelte components events</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/text-inputs">Svelte inputs</a>
-        <a class="dropdown-menu__dropdown-element" href="https://learn.svelte.dev/tutorial/onmount">Svelte onMount</a>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/welcome-to-svelte">Svelte tutorial</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/reactive-declarations">Svelte states</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/declaring-props">Svelte props and imports</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/default-values">Svelte default props</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/spread-props">Svelte spread props</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/else-if-blocks">Svelte if/else</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/each-blocks">Svelte loops/maps</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/keyed-each-blocks">Svelte keyed each</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/await-blocks">Svelte promise-handling</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/dom-events">Svelte events</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/event-modifiers">Svelte event modifiers</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/component-events">Svelte components events</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/text-inputs">Svelte inputs</a></p>
+        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/onmount">Svelte onMount</a></p>
       </div>
     </nav>
   </nav>
@@ -86,12 +88,15 @@
     position: relative;
     --dropdown-max-height: 0;
 
-    display: flex; flex-direction: column;
+    display: flex; flex-direction: column; align-items: center; justify-content: center;
   }
   .links-container__dropdown-container:hover {
     --dropdown-max-height: 60rem;
   }
   /* .links-container__dropdown-header {} */
+  .links_container__invisible-dropdown-wrapper {
+    position: absolute;
+  }
   .links-container__dropdown-menu {
     width: 8rem; max-height: var(--dropdown-max-height); 
 
@@ -99,8 +104,7 @@
     flex-direction: column;
     flex-wrap: nowrap;
 
-    position: absolute;
-    top: 1rem;
+    position: absolute; top: 1rem;
     overflow-y: hidden;
     background-color: var(--medium-light-blue);
   }
--- Frontend/src/routes/web-cam-test/+page.svelte
+++ Frontend/src/routes/web-cam-test/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
+  let stream: MediaStream, videoRef: HTMLVideoElement;
 
-  let stream: MediaStream;
-  let videoRef: HTMLVideoElement;
-
-  //$: console.log({videoRef})
+  $: showSuggestedAnswer = false
+  $: showAddAnswerForm = false
+  $: newAnswer = ""
 
   async function getStream() {
     try {
@@ -15,7 +15,6 @@
     } catch (err) {
       console.error(err);
     }
-    //console.log(stream.getTracks()[0])
   }
 
   async function stopStream() {
@@ -25,21 +24,66 @@
 </script>
 
 <div class="about-page">
-  <h2>Webcam Test!</h2>
+  <h2 class="about-page__header">Webcam Test!</h2>
 
-  <div>
-    {#if videoRef?.srcObject === null}
-      <button class="rounded-sm bg-slate-600 text-white px-4 py-2" on:click={getStream}>Start Stream</button>
-    {:else}
-      <button class="rounded-sm bg-red-600 text-white px-4 py-2" on:click={stopStream}>Stop Stream</button>
+  <video class={`mt-4 rounded-sm ${videoRef?.srcObject === null ? "--stream-closed" : "--stream-open"}`} autoplay={true} bind:this={videoRef}><track kind="captions"></video>
+
+  {#if videoRef?.srcObject === null}
+    <button class="start-stop-btn rounded-sm bg-slate-600 text-white" on:click={getStream}>Start Stream</button>
+  {:else}
+    <button class="start-stop-btn rounded-sm bg-red-600 text-white" on:click={stopStream}>Stop Stream</button>
+  {/if}
+
+  <section style="margin-top: 4rem; display: flex; flex-direction: column; align-items:center;">
+    <h3>This is the the QnA section</h3>
+
+    <p style="margin-top: 2rem;"><i>"Here will the question be, can you imagine it?"</i></p>
+
+    {#if showSuggestedAnswer}
+      <p style="margin-top: 2rem;">- "Yes I can!"</p>
     {/if}
+    <button class="start-stop-btn" style="margin-top: 1rem;" on:click={() => showSuggestedAnswer = !showSuggestedAnswer}>
+      {showSuggestedAnswer ? "close answer": "show a suggested answer"}
+    </button>
 
-    <video class="mt-4 rounded-sm " width="640" height="480" autoplay={true} bind:this={videoRef} />
-  </div>
+    {#if showAddAnswerForm}
+      <textarea placeholder="...write answer here" class="text-area-input" bind:value={newAnswer}/>
+    {/if}
+    <button class="start-stop-btn" style="margin-top: 1rem;" on:click={() => showAddAnswerForm = !showAddAnswerForm}>
+      {showAddAnswerForm ? "close answer form": "add an answer"}
+    </button>
+
+    <button class="rounded-sm start-stop-btn" disabled style="margin-top: 4rem;">Next quetion</button>
+  </section>
 </div>
 
 <style>
   .about-page {
+    padding-top: 3rem;
+    display: flex; flex-direction: column; align-items: center;
+  }
+  .about-page__header {
+    margin-bottom: 2rem;
+  }
+  .start-stop-btn {
+    margin-top: 2rem;
+    padding: 0.2rem 0.5rem;
     display: block;
+    cursor: pointer;
+  }
+  .--stream-closed {
+    height: 0px;
+  }
+  .--stream-open {
+    width: 640px;
+    height: 480px;
+  }
+  .text-area-input {
+    border-radius: 0.3rem;
+    padding: 0.5rem;
+    margin-top: 3rem;
+    text-wrap: wrap;
+    width: 100%; height: 3rem;
+    resize: vertical;
   }
 </style>
