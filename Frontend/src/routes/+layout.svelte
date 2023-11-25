c0ad29783221e4fd4fda1f020216b1762bc9909a
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-11-25 02:01:20 +0100

    Componotized dropdown menu

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -2,48 +2,58 @@
   import '../styles/base.css'
   import '../styles/colors.css'
   import '../styles/fonts.css'
+
+  import DropdownMenu from './components/DropdownMenu.svelte'
+
+  const navbarData = [
+    { 
+      menuTitle: "Site navigation",
+      menuItems: [
+        { title: "Preparation", path: "/preparation" },
+        { title: "Webcam test", path: "/web-cam-test" },
+        { title: "About", path: "/about" },
+      ]
+    },
+    { 
+      menuTitle: "Svelte Tutorial",
+      menuItems: [
+        { title: "Svelte tutorial start", path: "https://learn.svelte.dev/tutorial/welcome-to-svelte" },
+        { path: "https://learn.svelte.dev/tutorial/welcome-to-svelte", title: "Svelte tutorial" },
+        { path: "https://learn.svelte.dev/tutorial/reactive-declarations", title: "Svelte states" },
+        { path: "https://learn.svelte.dev/tutorial/declaring-props", title: "Svelte props and imports" },
+        { path: "https://learn.svelte.dev/tutorial/default-values", title: "Svelte default props" },
+        { path: "https://learn.svelte.dev/tutorial/spread-props", title: "Svelte spread props" },
+        { path: "https://learn.svelte.dev/tutorial/else-if-blocks", title: "Svelte if/else" },
+        { path: "https://learn.svelte.dev/tutorial/each-blocks", title: "Svelte loops/maps" },
+        { path: "https://learn.svelte.dev/tutorial/keyed-each-blocks", title: "Svelte keyed each" },
+        { path: "https://learn.svelte.dev/tutorial/await-blocks", title: "Svelte promise-handling" },
+        { path: "https://learn.svelte.dev/tutorial/dom-events", title: "Svelte events" },
+        { path: "https://learn.svelte.dev/tutorial/event-modifiers", title: "Svelte event modifiers" },
+        { path: "https://learn.svelte.dev/tutorial/component-events", title: "Svelte components events" },
+        { path: "https://learn.svelte.dev/tutorial/text-inputs", title: "Svelte inputs" },
+        { path: "https://learn.svelte.dev/tutorial/onmount", title: "Svelte onMount" },
+      ]
+    },
+    { 
+      menuTitle: "Other Tutorials",
+      menuItems: [
+        { title: "Webcam + Svelte", path: "https://dev.to/nelsoncode/how-to-use-the-webcam-with-svelte-js-2415" },
+        { title: "Links css", path: "https://codepen.io/martinwolf/pen/eNNdme" },
+        { title: "Color palette", path: "https://colorhunt.co/palette/fefbe9e1eeddf0a04b183a1d" },
+      ]
+    },
+  ]
+
 </script>
 
 
 <header class="site__header">
   <h1><a href="/">Site Title E</a></h1>
+
   <nav class="header__links-container">
-    <nav class="links-container__dropdown-container dev-border">
-      <h4 class="links-container__dropdown-header">Site Navigation</h4>
-      
-      <div class="links-container__dropdown-menu">
-        <p class="dropdown-menu__dropdown-element"><a href="/preparation">/preparation</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="/web-cam-test">/webcam test</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="/about">/about</a></p>
-      </div>
-    </nav>
-    <nav class="links-container__dropdown-container">
-      <h4 class="links-container__dropdown-header">Other Tutorials</h4>
-      <div class="links-container__dropdown-menu">
-        <p class="dropdown-menu__dropdown-element"><a href="https://dev.to/nelsoncode/how-to-use-the-webcam-with-svelte-js-2415">Webcam + Svelte</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://codepen.io/martinwolf/pen/eNNdme">Links css</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://colorhunt.co/palette/fefbe9e1eeddf0a04b183a1d">Color palette</a></p>
-      </div>
-    </nav>
-    <nav class="links-container__dropdown-container">
-      <h4 class="links-container__dropdown-header">Svelte Tutorial</h4>
-      <div class="links-container__dropdown-menu">
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/welcome-to-svelte">Svelte tutorial</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/reactive-declarations">Svelte states</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/declaring-props">Svelte props and imports</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/default-values">Svelte default props</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/spread-props">Svelte spread props</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/else-if-blocks">Svelte if/else</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/each-blocks">Svelte loops/maps</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/keyed-each-blocks">Svelte keyed each</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/await-blocks">Svelte promise-handling</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/dom-events">Svelte events</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/event-modifiers">Svelte event modifiers</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/component-events">Svelte components events</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/text-inputs">Svelte inputs</a></p>
-        <p class="dropdown-menu__dropdown-element"><a href="https://learn.svelte.dev/tutorial/onmount">Svelte onMount</a></p>
-      </div>
-    </nav>
+    {#each navbarData as navItem (navItem.menuTitle)}
+      <DropdownMenu title={navItem.menuTitle} menuItems={navItem.menuItems}/>
+    {/each}
   </nav>
 </header>
 
@@ -83,35 +93,5 @@
     justify-content: center;
     align-items: center;
     gap: 1rem;
-  }
-  .links-container__dropdown-container {
-    position: relative;
-    --dropdown-max-height: 0;
-
-    display: flex; flex-direction: column; align-items: center; justify-content: center;
-  }
-  .links-container__dropdown-container:hover {
-    --dropdown-max-height: 60rem;
-  }
-  /* .links-container__dropdown-header {} */
-  .links_container__invisible-dropdown-wrapper {
-    position: absolute;
-  }
-  .links-container__dropdown-menu {
-    width: 8rem; max-height: var(--dropdown-max-height); 
-
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-
-    position: absolute; top: 1rem;
-    overflow-y: hidden;
-    background-color: var(--medium-light-blue);
-  }
-  .dropdown-menu__dropdown-element {
-    width: 100%;
-    padding: 0.3rem 0.5rem;
-    border: 1px solid black;
-    text-align: center;
   }
 </style>
---
+++ Frontend/src/routes/components/DropdownMenu.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  export let title: string
+  export let menuItems: {
+    title: string,
+    path: string
+  }[]
+</script>
+
+
+<nav class="links-container__dropdown-container dev-border">
+  <h4 class="links-container__dropdown-header">{title}</h4>
+  
+  <div class="links-container__dropdown-menu">
+    {#each menuItems as menuItem (menuItem.title)}
+      <p class="dropdown-menu__dropdown-element">
+        <a href={menuItem.path}>{menuItem.title}</a>
+      </p>
+    {/each}
+  </div>
+</nav>
+
+
+<style>
+ .links-container__dropdown-container {
+    position: relative;
+    --dropdown-max-height: 0;
+
+    display: flex; flex-direction: column; align-items: center; justify-content: center;
+  }
+  .links-container__dropdown-container:hover {
+    --dropdown-max-height: 60rem;
+  }
+  /* .links-container__dropdown-header {} */
+  /* .links_container__invisible-dropdown-wrapper {
+    position: absolute;
+  } */
+  .links-container__dropdown-menu {
+    width: 8rem; max-height: var(--dropdown-max-height); 
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+
+    position: absolute; top: 1rem;
+    overflow-y: hidden;
+    background-color: var(--medium-light-blue);
+  }
+  .dropdown-menu__dropdown-element {
+    width: 100%;
+    padding: 0.3rem 0.5rem;
+    border: 1px solid black;
+    text-align: center;
+  }
+</style>
