8fa20402374c42f8c30bfbf3990a07f3853fa82c
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-11-28 17:42:41 +0100

    should have been included in last commit

--- backend/API/main.py
+++ backend/API/main.py
@@ -1,7 +1,24 @@
 import json
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
+from ariadne.explorer.playground import PLAYGROUND_HTML
+from ariadne import load_schema_from_path, make_executable_schema, \
+    snake_case_fallback_resolvers, graphql_sync, ObjectType
+from flask_cors import CORS
+from data.graphQl.resolvers import get_data_resolver
+
+query = ObjectType("Query")
+query.set_field("getData", get_data_resolver)
 
 app = Flask(__name__)
+CORS(app)
+
+schema_from_file = load_schema_from_path("../data/graphQl/schema.graphql")
+refined_schema = make_executable_schema(
+    schema_from_file,
+    query,
+    snake_case_fallback_resolvers
+)
 
 
 @app.route('/')
@@ -9,7 +26,26 @@ def hello():
     return "Hey"
 
 
-@app.route('/api/data/<file_name>')
+# https://studio.apollographql.com/sandbox/explorer
+@app.get('/api/graphql/v1/')
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+
+@app.post('/api/graphql/v1/')
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        refined_schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+@app.get('/api/data/<file_name>')
 def get_api_data(file_name):
     try:
         json_file = open('../data/json/data.{}.json'.format(file_name))
